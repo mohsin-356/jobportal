6cf8453e28882b2212537ef3a1e84d5d61f0a2cf
mohsin-356 <mohsinsaeed356@gmail.com>
2025-07-12 15:14:25 +0500

    Import AppliedJobTable component and display applied jobs in Profile component

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button'
 import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
-// import AppliedJobTable from './AppliedJobTable'
+import AppliedJobTable from './AppliedJobTable'
 // import UpdateProfileDialog from './UpdateProfileDialog'
 import { useSelector } from 'react-redux'
 import Footer from './shared/Footer'
@@ -67,8 +67,7 @@ const Profile = () => {
       <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
         <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
         {/* Applied Job Table   */}
-        {/* <AppliedJobTable /> */}
-        <p>Applied Jobs table will be here</p>
+        <AppliedJobTable />
       </div>
       {/* <UpdateProfileDialog open={open} setOpen={setOpen}/> */}
       <p>Update Profile Dialog will be here</p>
