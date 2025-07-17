8bae5ee5976b9ce074c23d4de3c19c8ebeac8705
mohsin-356 <mohsinsaeed356@gmail.com>
2025-07-17 17:04:05 +0500

    Fix job data retrieval in useGetAllJobs hook and ensure it is called in Home component

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -5,13 +5,12 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 
 const useGetAllJobs = () => {
-    const dispatch=useDispatch();
-    useEffect(()=>{
-        const fetchAllJobs=async()=>{
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchAllJobs = async () => {
             try {
-                const res=await axios.get(`${JOB_API_END_POINT}/get`,{withCredentials:true});
-                if (res.data.success)
-                {
+                const res = await axios.get(`${JOB_API_END_POINT}/get`, { withCredentials: true });
+                if (res.data.success) {
                     dispatch(setAllJobs(res.data.jobs));
                 }
             } catch (error) {
@@ -19,7 +18,7 @@ const useGetAllJobs = () => {
             }
         }
         fetchAllJobs();
-    },[]);
+    }, []);
 }
 
 export default useGetAllJobs
